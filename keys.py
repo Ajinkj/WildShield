a973bbe17e638137032c3587dfb856b0fa27a67c
Ajin k j <61120485+Ajinkj@users.noreply.github.com>
2022-07-16 20:02:27 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,9 +1,5 @@
-account_sid='AC5b29ea1e6f4d4a3f9a275061b395b207'
-auth_token='0c00e123e18d2da90ba83c3c1cfc9b86'
+account_sid=
+auth_token=
 
-twilio_number='+19854015401'
-target_number= '+918078039407'
-
-acc=AC0ae5c37d6dbe5b13c684e730ee13a19b
-auth="auth:78fc755589dc3f4d2ea074cf37ed7111"
-ph=+12512764649
+twilio_number=
+target_number= 
