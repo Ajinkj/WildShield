b33ff7b381dedb4d4945b17b319ffa1629485a36
Ajin k j <61120485+Ajinkj@users.noreply.github.com>
2022-07-16 18:47:45 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -3,3 +3,7 @@ auth_token='0c00e123e18d2da90ba83c3c1cfc9b86'
 
 twilio_number='+19854015401'
 target_number= '+918078039407'
+
+acc=AC0ae5c37d6dbe5b13c684e730ee13a19b
+auth=25badb98fc49b02b6ba9d26127d6b20c
+ph=+12512764649
